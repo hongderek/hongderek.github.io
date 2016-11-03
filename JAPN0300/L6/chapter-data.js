2a037230c5b1af2fe6f5713bcc7aa80362d76075
hongderek <hong.derek.j@gmail.com>
2016-11-03 01:45:07 -0400

    ASDF

---
+++ JAPN0300/L6/chapter-data.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const local_course_name = '日本語３００';
+
+const local_chap_name = '第６課';
+
+const chap_kanji_str = 'kanji,reading,meaning\nasdf,asdf,asdf';
+
+const chap_grammar_list = [
+{
+  grammar_point: 'asdf',
+  spec: 'asdf',
+  meaning: 'asdf',
+  use: 'asdf',
+  example: 'asdf',
+  other: 'asdf'
+}
+];
+
+/**
+{
+  grammar_point: '',
+  spec: '',
+  meaning: '',
+  use: '',
+  example: '',
+  other: ''
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
