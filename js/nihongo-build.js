67468d77f434b50c7c01c136d0c4a2381b527f3a
hongderek <hong.derek.j@gmail.com>
2016-11-04 20:04:16 -0400

    ASDF

--- js/nihongo-build.js
+++ js/nihongo-build.js
@@ -19,7 +19,7 @@ $(window).on('load', function() {
     '</div>'
     ].join('\n');
     var course_nav_entry = [
-    '  <li class="sidebar-subtopic"><a href="#nihongo-course' + i + '">' + cl_course_name + '</a></li>'
+    '  <li class="sidebar-subtopic"><a href="#nihongo-course-' + i + '">' + cl_course_name + '</a></li>'
     ].join('\n');
 
     $(course_nav_id).append(course_nav_entry);
