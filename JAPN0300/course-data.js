42b91644d695d3d0837b7298aa30c24a4f8b9ed9
hongderek <hong.derek.j@gmail.com>
2016-11-14 22:55:27 -0500

    build 7.0

--- JAPN0300/course-data.js
+++ JAPN0300/course-data.js
@@ -4,25 +4,23 @@ const local_chap_list = [
 {
   name: '第１課',
   path: 'L1'
-},
-{
+},{
   name: '第２課',
   path: 'L2'
-},
-{
+},{
   name: '第３課(wip)',
   path: 'L3'
-},
-{
+},{
   name: '第４課(wip)',
   path: 'L4'
-},
-{
+},{
   name: '第５課(wip)',
   path: 'L5'
-},
-{
+},{
   name: '第６課',
   path: 'L6'
+},{
+  name: '第７課',
+  path: 'L7(wip)'
 }
 ];
