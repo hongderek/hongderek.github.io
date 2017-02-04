ee9bf076e940da01cb68bfc5d3c1c518d9f307cb
hongderek <hong.derek.j@gmail.com>
2017-02-03 20:23:43 -0500

    ASDF

--- JAPN0400/course-data.js
+++ JAPN0400/course-data.js
@@ -4,5 +4,20 @@ const local_chap_list = [
 {
   name: '第８課',
   path: 'L8'
+},{
+  name: '第９課',
+  path: 'L9'
+},{
+  name: '第１０課',
+  path: 'L10'
+},{
+  name: '第１１課',
+  path: 'L11'
+},{
+  name: '第１２課',
+  path: 'L12'
+},{
+  name: '第１３課',
+  path: 'L13'
 }
 ];
